be8632ab87bbe2e8829b2926903acb6b787b1fc4
K1ngK171 <s11847233@students.duvalschools.org>
2024-09-10 13:20:18 -0400

    new project start

---
+++ 01_gaming_exercises/00_awarding_extra_lives/AwardingExtraLives.py
@@ -0,0 +1,38 @@
+# Awarding Extra Lives, Roy Smith IV, v0.0
+
+score = 0
+lives = 3
+
+# Allow the user to input the score AS AN INTEGER
+# If score is 10000 or less
+    # Lose a Life
+# If score is > 10000 but less than 100001
+    # Gives 1 Extra Life
+# If score is > 100000 
+    # Gain 2 Extra Lives
+
+# Output the score and nuimber if lives on screen
+
+if score > 100:
+    print("it is hot as the sun outside.\n")
+
+if score >= 10000:
+    print("can i got home?\n")
+
+if score:
+    print("Disgusting")
+
+
+if score == "1":
+    print("Your shirt is the correct uniform shirt.\n")
+else:
+    print("Your shirt is not the correct uniform shirt.\n")
+
+if score >= 6:
+    print("I wear a shoe size of 8.\n")
+else:
+    print("I dont wear a shoe size of 8.\n")
+
+
+
+
